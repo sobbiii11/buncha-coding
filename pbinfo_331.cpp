5c63d7b09efa669794a5c1daef251caa84d3f5b0
Gabriel Gruia <79210187+ggruia@users.noreply.github.com>
2025-03-07 18:39:29 +0200

    for -> while

--- pbinfo_331.cpp
+++ pbinfo_331.cpp
@@ -6,9 +6,10 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    
-    for (int i = n * 2 - 1; i >= 1; i -= 2) {
-        cout << i << ' ';
+
+    while(n) {
+        cout << n * 2 - 1 << " ";
+        n--;
     }
     
     return 0;
