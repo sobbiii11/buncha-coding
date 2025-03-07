a1f213ad1013353809ad4a07f0868b05e50911b0
Gabriel Gruia <79210187+ggruia@users.noreply.github.com>
2025-03-07 18:29:42 +0200

    code style

--- pbinfo_1362.cpp
+++ pbinfo_1362.cpp
@@ -1,14 +1,15 @@
 // https://www.pbinfo.ro/solutii/user/gruia_cristian/problema/1362/p10
 #include <iostream>
-#include <cstdlib>
+
 using namespace std;
 
-int main()
-{        
+int main() {        
     int n;
-        cin >> n;
-        cout << 1;
-        for (int i = 1; i <= n; ++i)
-            cout << 0;
+    cin >> n;
+    
+    cout << 1;
+    for (int i = 1; i <= n; ++i)
+        cout << 0;
+    
     return 0;
 }
