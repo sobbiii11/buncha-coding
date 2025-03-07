1cf7805e2fa691aff50425ef8c806f31a74f1664
sobbiii11 <gruia.cristian1812@gmail.com>
2025-03-07 22:02:13 +0200

    Add files via upload

---
+++ pbinfo_939.cpp
@@ -0,0 +1,12 @@
+//https://www.pbinfo.ro/probleme/939/sum00
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    
+    cin >> a >> b;
+    cout << a + b;
+    
+    return 0;
+}
